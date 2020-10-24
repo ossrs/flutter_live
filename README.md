4584df23dd7e029e2e754d4e796c4de91c41fe28
winlin <winlin@vip.126.com>
2020-10-24 20:49:16 +0800

    Release 1.0.1, refine demo

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 
 Live streaming demo by Flutter+[SRS](https://github.com/ossrs/srs).
 
-Coming soon...
+![Home](./images/01-home-02.jpg)
+
+![Home](./images/02-show-01.jpg)
+
+![Home](./images/03-realtime.jpg)
+
+> Warning: Not support simulator, [#14647](https://github.com/flutter/flutter/issues/14647).
 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:flutter_live/flutter_live.dart';
-
-import 'home.dart';
+import 'package:fijkplayer/fijkplayer.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,28 +18,6 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterLive.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-      print('Running on $_platformVersion');
-    });
   }
 
   @override
@@ -50,5 +25,157 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Home()
     );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SRS: Flutter Live Streaming'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextField(
+            controller: _controller,
+            autofocus: false,
+          ),
+          RadioListTile(
+            title: Text('RTMP'),
+            subtitle: Text(FlutterLive.rtmp),
+            value: FlutterLive.rtmp,
+            selected: _controller.text == FlutterLive.rtmp,
+            groupValue: _controller.text,
+            onChanged: (v) {
+              setState(() {
+                updateUrls(v);
+              });
+            },
+          ),
+          RadioListTile(
+            title: Text('HLS'),
+            value: FlutterLive.hls,
+            subtitle: Text(FlutterLive.hls),
+            selected: _controller.text == FlutterLive.hls,
+            groupValue: _controller.text,
+            onChanged: (v) {
+              setState(() {
+                updateUrls(v);
+              });
+            },
+          ),
+          RadioListTile(
+            title: Text('HTTP-FLV'),
+            subtitle: Text(FlutterLive.flv),
+            value: FlutterLive.flv,
+            selected: _controller.text == FlutterLive.flv,
+            groupValue: _controller.text,
+            onChanged: (v) {
+              setState(() {
+                updateUrls(v);
+              });
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              RaisedButton(
+                child: Text('Play'),
+                onPressed: () {
+                  startPlay(context);
+                },
+              ),
+              Container(
+                width: 10,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void updateUrls(String v) {
+    _controller.text = v;
+  }
+
+  void startPlay(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return VideoPlayer(url: _controller.text);
+    }));
+  }
+}
+
+class VideoPlayer extends StatefulWidget {
+  final String url;
+
+  VideoPlayer({@required this.url});
+
+  @override
+  _VideoPlayerState createState() => _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  final FijkPlayer player = FijkPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    player.setOption(FijkOption.playerCategory, "mediacodec-all-videos", 1);
+    startPlay();
+  }
+
+  void startPlay() async {
+    print('Start play ${widget.url}');
+
+    await player.setOption(FijkOption.hostCategory, "request-screen-on", 1);
+    await player.setOption(FijkOption.hostCategory, "request-audio-focus", 1);
+
+    // Live low-latency: https://www.jianshu.com/p/d6a5d8756eec
+    // For all options, read https://github.com/Bilibili/ijkplayer/blob/master/ijkmedia/ijkplayer/ff_ffplay_options.h
+    await player.setOption(FijkOption.formatCategory, "probesize", 16 * 1024); // in bytes
+    await player.setOption(FijkOption.formatCategory, "analyzeduration", 100 * 1000); // in us
+    await player.setOption(FijkOption.playerCategory, "packet-buffering", 0); // 0, no buffer.
+    await player.setOption(FijkOption.playerCategory, "max_cached_duration", 800); // in ms
+    await player.setOption(FijkOption.playerCategory, "max-buffer-size", 32 * 1024); // in bytes
+    await player.setOption(FijkOption.playerCategory, "infbuf", 1); // 1 for realtime.
+    await player.setOption(FijkOption.playerCategory, "min-frames", 1); // in frames
+
+    await player.setDataSource(widget.url, autoPlay: true).catchError((e) {
+      print("setDataSource error: $e");
+    });
+
+    player.enterFullScreen();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('SRS Live Streaming'),
+        ),
+        body: Container(
+            child: FijkView(
+              player: player,
+              panelBuilder: fijkPanel2Builder(),
+              fsFit: FijkFit.fill,
+            )
+        )
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    player.release();
   }
 }
