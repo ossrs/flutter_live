8cc5dc3059c2333d9c61fc684eb18c4bbe24e47b
winlin <winlin@vip.126.com>
2020-10-27 17:26:56 +0800

    Remove debug banner

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -26,6 +26,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home()
     );
   }
