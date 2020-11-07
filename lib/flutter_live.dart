916e4c9ac3a61788086792aa7e31fa3b40601b1b
winlin <winlin@vip.126.com>
2020-11-07 22:48:56 +0800

    Improve scores

--- lib/flutter_live.dart
+++ lib/flutter_live.dart
@@ -4,18 +4,27 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterLive {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_live');
+  // The channel for platform.
+  static const MethodChannel _channel = const MethodChannel('flutter_live');
 
+  // Get the platform information.
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  // Demo streams by https://ossrs.net/
+  // RTMP demo stream by https://ossrs.net/
   static const String rtmp = 'rtmp://r.ossrs.net/live/livestream';
+
+  // HLS demo stream by https://ossrs.net/
   static const String hls = 'http://r.ossrs.net/live/livestream.m3u8';
+
+  // HTTP-FLV demo stream by https://ossrs.net/
   static const String flv = 'http://r.ossrs.net/live/livestream.flv';
+
+  // HTTPS-FLV demo stream by https://ossrs.net/
   static const String flvs = 'https://d.ossrs.net:18088/live/livestream.flv';
+
+  // HTTPS-HLS demo stream by https://ossrs.net/
   static const String hlss = 'https://d.ossrs.net:18088/live/livestream.m3u8';
 }
